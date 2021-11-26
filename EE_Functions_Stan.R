06db00af220e0a40d180f6e806fdc36cd44b5606
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2021-11-26 12:18:25 -0600

    Update EE_Functions_Stan.R

--- EE_Functions_Stan.R
+++ EE_Functions_Stan.R
@@ -10,9 +10,11 @@
 if (exists("env_code")) detach(env_code)
 if (exists("env_modfun")) detach(env_modfun)
 if (exists("env_eb")) detach(env_eb)
+if (exists("env_stan")) detach(env_stan)
 env_code <- new.env(parent = emptyenv())
 env_modfun <- new.env(parent = emptyenv())
 env_eb <- new.env(parent = emptyenv())
+env_stan <- new.env(parent = emptyenv())
 
 ###############  Coding Functions Environment ################ 
 env_code$setup_cores <- function(ncores){
@@ -553,8 +555,8 @@ env_modfun$logpdf_mvt <- function(beta, alpha, cov_inv, df = 100) {
   return(result)
 }
 
-###############  EB Functions Environment ###############
-env_eb$prep_file_stan <- function(idtaskdep, indcode_list, train = TRUE, other_data = NULL) {
+###############  Stan Functions Environment ###############
+env_stan$prep_file_stan <- function(idtaskdep, indcode_list, train = TRUE, other_data = NULL) {
   sort_order <- order(idtaskdep[, 1], idtaskdep[, 2])
   sort_order[!train] <- 0 # Non-training gets order = 0, which removes
   ind <- as.matrix(indcode_list$indcode[sort_order,])
@@ -622,6 +624,85 @@ env_eb$prep_file_stan <- function(idtaskdep, indcode_list, train = TRUE, other_d
   }  
 }
 
+env_stan$checkconverge_export <- function(data_stan, nchains, dir_stanout, outname, out_prefix, dir_work){
+  cat("Reading draws from Stan csv output into R (large files take time)...")
+  csv_name <- do.call(c, lapply(1:nchains, function(i) paste0(outname,"-",i,".csv")))
+  draws_beta <- read_cmdstan_csv(file.path(dir_stanout, csv_name), variables = "beta_ind", format = "draws_list", sampler_diagnostics = "accept_stat__")
+  cat("DONE")
+  
+  ### Save output files and check convergence ###
+  draws_name <- paste0(out_prefix,"_draws_beta.rds")
+  util_name <- paste0(out_prefix,"_utilities_r.csv")
+  pdf_name <- paste0(out_prefix,"_trace_plots.pdf")
+  fit_name <-  paste0(out_prefix,"_fit_stats.csv")
+  message(paste0(
+    "\nSaving post warm-up files for:\n",
+    " respondent point estimates:    ", util_name,"\n",  
+    " draws of utilities as R list:  ", draws_name,"\n",
+    " convergence stats of mean:     ", fit_name, "\n",
+    " PDF of detailed traceplots:    ", pdf_name,"\n",
+    "\nShowing post warm-up:\n",
+    " Acceptance rate across iterations (histogram)\n",
+    " Traceplots of all mean utilities together (Sawtooth chart)"
+  ))
+  
+  hist(do.call(rbind,draws_beta$post_warmup_sampler_diagnostics)$accept_stat__, breaks = 30, main = "Acceptance Rate - Sampling", xlab = "", xlim = c(0,1))
+  saveRDS(modifyList(draws_beta,list(warmup_draws = NULL)), file.path(dir_work, draws_name)) # drop warmup
+  utilities <- matrix(
+    Reduce("+",lapply(draws_beta$post_warmup_draws, colMeans))/nchains,
+    data_stan$I, data_stan$P,
+    byrow = TRUE) # First P entries are respondent 1, next P are resp 2
+  utilities_r <- utilities %*% t(data_stan$code_master)
+  write.table(cbind(id = data_stan$resp_id, utilities_r), file = file.path(dir_work, util_name), sep = ",", na = ".", row.names = FALSE)
+  
+  # Convergence charts saved as pdf and in fit_stats
+  fit_stats <- data.frame(
+    variable = colnames(data_stan$ind),
+    mean = NA,
+    rhat = NA,
+    ESS = NA
+  )
+  ndraws <- nrow(draws_beta$post_warmup_draws[[1]])
+  draws_beta_mu <- list() # Creates the mean of respondent utilities for each iteration, like alpha
+  for (chain_i in (1:nchains)){
+    draws_beta_list <- as.matrix(draws_beta$post_warmup_draws[[chain_i]])
+    draws_beta_mu[[chain_i]] <- t(sapply(1:ndraws, function(draw){
+      beta_mu <- colMeans(matrix(draws_beta_list[draw,],
+                                 data_stan$I,data_stan$P, byrow = TRUE))
+    }))
+    matplot(1:nrow(draws_beta_mu[[chain_i]]), draws_beta_mu[[chain_i]],
+            type = "l" , lty = 1, lwd = 1, main = paste0("Chain ", chain_i), xlab = "Iteration", ylab = "Mean Beta")   
+  } 
+  
+  pdf(file = file.path(dir_work, pdf_name),   # The directory you want to save the file in
+      width = 7, # The width of the plot in inches
+      height = 5) # The height of the plot in inches
+  hist(do.call(rbind,draws_beta$post_warmup_sampler_diagnostics)$accept_stat__, breaks = 30, main = "Acceptance Rate - Sampling", xlab = "", xlim = c(0,1))
+  for (chain_i in (1:nchains)){
+    matplot(1:nrow(draws_beta_mu[[chain_i]]), draws_beta_mu[[chain_i]],
+            type = "l" , lty = 1, lwd = 1, main = paste0("Chain ", chain_i), xlab = "Iteration", ylab = "Mean Beta")   
+  }  
+  chain_cols <- c("red","blue","green","black")
+  for (i in 1:ncol(draws_beta_mu[[1]])){
+    x <- sapply(1:length(draws_beta_mu), function(chain){
+      draws_beta_mu[[chain]][,i]     
+    }) # x is set of column i across draws_beta_mu
+    fit_stats$mean[i] <- round(mean(x), 2)
+    fit_stats$rhat[i] <- round(rhat(x),2)
+    fit_stats$ESS[i] <- round(ess_basic(x),1)
+    plot(x[,1], type = "l", col = chain_cols[1], ylim = c(min(x), max(x)),
+         xlab = "Sample Iteration", ylab = "Mean Beta",
+         main = paste(colnames(data_stan$ind)[i],
+                      "| rhat = ", round(rhat(x),2),
+                      "| ESS = ", round(ess_basic(x),1)
+         ))
+    for (chain in 2:nchains){
+      lines(x[,2], type = "l", col = chain_cols[chain])
+    }
+  }
+  dev.off()
+  write.table(fit_stats, file = file.path(dir_work, paste0(out_prefix,"_fit_stats.csv")), sep = ",", na = ".", row.names = FALSE)
+}
 
 env_eb$numder_2 <- function(x, pos, delta = .01){
   # 2nd derivative
@@ -1172,6 +1253,7 @@ env_eb$JeffPrior <- function(xvec, data_list, model_list, ID_Var = FALSE, ObsFis
 attach(env_code)
 attach(env_modfun)
 attach(env_eb)
+attach(env_stan)
 
 # 1) Scale as mean(previous prior,  scale * current) 
 # 2) Future test Likelihood of beta is average of likelihoods from multiple Priors
