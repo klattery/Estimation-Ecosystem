1d210a5bdb56ac7272aefd58ee0ef696ae2c8b60
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2021-12-04 12:08:10 -0600

    Update EE_Functions_Stan2.R

--- EE_Functions_Stan2.R
+++ EE_Functions_Stan2.R
@@ -680,7 +680,7 @@ env_stan$stan_compile_and_est <- function(data_stan, data_model, dir_stanmodel,s
 }
 
 
-env_stan$checkconverge_export <- function(data_stan, nchains, dir_stanout, outname, out_prefix, dir_work, r_cores){
+env_stan$checkconverge_export <- function(data_stan, nchains, dir_stanout, outname, out_prefix, dir_work){
   cat("Reading draws from Stan csv output into R (large files take time)...")
   csv_name <- do.call(c, lapply(1:nchains, function(i) paste0(outname,"-",i,".csv")))
   draws_beta <- read_cmdstan_csv(file.path(dir_stanout, csv_name), variables = "beta_ind", format = "draws_list", sampler_diagnostics = "accept_stat__")
