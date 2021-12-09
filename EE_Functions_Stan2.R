a1b8582d7e2f39a670d4d974bfc5a3f15c4f06d5
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2021-12-09 13:27:38 -0600

    Update EE_Functions_Stan2.R

--- EE_Functions_Stan2.R
+++ EE_Functions_Stan2.R
@@ -701,6 +701,7 @@ env_stan$stan_compile_and_est <- function(data_stan, data_model, dir_stanmodel,s
   )
   gc()
 }
+
 env_stan$message_estimation(){
   cat("While Stan runs, you may check progress in terminal:\n")
   message(paste0("cd ", dir_stanout, "   # Change to your working directory and then:\n",
