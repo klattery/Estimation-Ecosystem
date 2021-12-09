975e71c8edfb8e1bd718cbd6edd055594de55275
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2021-12-09 15:38:22 -0600

    Update EE_Functions_Stan2.R

--- EE_Functions_Stan2.R
+++ EE_Functions_Stan2.R
@@ -20,17 +20,17 @@ env_stan <- new.env(parent = emptyenv())
 env_code$read_data <- function(dir_data, data_file){
   if (!file.exists(file.path(dir_data, data_file))){
     message(paste0("Cannot find your data file: ", data_file)) 
-    data_in <- NULL
+    result <- NULL
   } else {
     ftype <- toupper(substr(data_file,nchar(data_file)-2, nchar(data_file)))
     if (ftype %in% c("CSV", "RDS")){
-      if (ftype == "CSV") {data_in <- read.csv(file.path(dir_data, data_file), as.is=TRUE)}
-      if (ftype == "RDS") {data_in <- readRDS(file.path(dir_data, data_file))}
+      if (ftype == "CSV") {result <- read.csv(file.path(dir_data, data_file), as.is=TRUE)}
+      if (ftype == "RDS") {result <- readRDS(file.path(dir_data, data_file))}
       message("READ DATA INTO R")
-      str(data_in)  
+      str(result)  
     } else {
       message("ERROR: MUST BE .CSV OR .RDS FILE")
-      data_in <- NULL
+      result <- NULL
     } 
   }
   return(data_in)
