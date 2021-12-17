65f19ac1b6393d52827d23d0bc3759c31fa3c5cc
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2021-12-16 19:43:53 -0600

    Update EE_Functions_Stan2.R

--- EE_Functions_Stan2.R
+++ EE_Functions_Stan2.R
@@ -952,9 +952,9 @@ env_stan$eb_betas_est <- function(data_stan, draws_beta, x0, r_cores, out_prefix
     preds <- do.call(rbind, lapply(result, function(x) x[[2]]))
   }
   if (nrow(betas_eb) < data_stan$I) cat("Some respondents not estimated for EB") 
-  utilities_r_eb <- betas_eb[,-1:-3]  %*% t(data_stan$code_master) # id, rlh_eb, rlh_mu
+  utilities_r_eb <- betas_eb[,-1:-2]  %*% t(data_stan$code_master) # id, rlh_eb
   util_eb_name <- paste0(out_prefix,"_utilities_r_eb.csv")
-  write.table(cbind(betas_eb[,1:3], utilities_r_eb), file = file.path(dir_work, util_eb_name), sep = ",", na = ".", row.names = FALSE)
+  write.table(cbind(betas_eb[,1:2], utilities_r_eb), file = file.path(dir_work, util_eb_name), sep = ",", na = ".", row.names = FALSE)
   message(paste0("\nEB point estimates in: ",util_eb_name))
   colnames(preds) <- c("id","task","dep","wts","pred_eb")
   preds_name <- paste0(out_prefix,"_preds.csv")
