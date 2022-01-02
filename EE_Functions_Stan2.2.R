8f0e7a71442a751fdeb57de28c80eb6ec7e85f8e
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2022-01-02 10:40:48 -0600

    Update EE_Functions_Stan2.2.R

--- EE_Functions_Stan2.2.R
+++ EE_Functions_Stan2.2.R
@@ -891,6 +891,7 @@ env_stan$est_agg_model <- function(data_list){
   agg_beta <- optim(par = model_agg$x0, fn = model_agg$func$min, gr = model_agg$func$gr, method ="BFGS",
                     data_list = data_stan, model_env = model_agg, control = list(maxit = 100, reltol = 1e-5, trace = 1, REPORT = 1))
   agg_beta_r <- data_stan$code_master %*% agg_beta$par
+  return(agg_beta_r)
 }
 
 env_stan$eb_betas_est <- function(data_stan, draws_beta, x0, r_cores, out_prefix, dir_work, cov_scale, linux = TRUE, nids_core = 5){
