c91369b21c8a6af4140574c4ff60985af6ac55a1
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2022-01-17 09:04:18 -0600

    Update EE_Functions_Stan2.2.R

--- EE_Functions_Stan2.2.R
+++ EE_Functions_Stan2.2.R
@@ -11,10 +11,12 @@ if (exists("env_code")) detach(env_code)
 if (exists("env_modfun")) detach(env_modfun)
 if (exists("env_eb")) detach(env_eb)
 if (exists("env_stan")) detach(env_stan)
+if (exists("env_shiny")) detach(env_shiny)
 env_code <- new.env(parent = emptyenv())
 env_modfun <- new.env(parent = emptyenv())
 env_eb <- new.env(parent = emptyenv())
 env_stan <- new.env(parent = emptyenv())
+env_shiny <- new.env(parent = emptyenv())
 
 ###############  Coding Functions Environment ################ 
 
@@ -1602,10 +1604,116 @@ env_eb$JeffPrior <- function(xvec, data_list, model_list, ID_Var = FALSE, ObsFis
   return(JeffPrior)
 }
 
+# Define UI for data upload app ----
+env_shiny$ui_1 <- fluidPage(
+  # App title ----
+  titlePanel("Upload files for HB into R"),
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      # Input: Select a file ----
+      fileInput("file_conjoint", "File with Choices (csv or rds)",
+                multiple = FALSE,
+                accept = c(".csv",".rds")),
+      fileInput("file_specs","File with Excel Specs",
+                multiple = FALSE,
+                accept = c(".xls",".xlsx")),
+      checkboxInput("use_cov", "Use Covariates (Optional)"),
+      conditionalPanel(
+        condition = "input.use_cov == true",
+        fileInput("file_cov", "Optional: File with Covariates",
+                  multiple = FALSE,
+                  accept = c(".csv",".rds"))),
+      tags$hr(),
+      # Checkboxes
+      checkboxInput("check_collinearity", "Check Collinearity", TRUE),
+      checkboxInput("est_aggmodel", "Estimate Aggregate Model", TRUE),
+      tags$hr(),
+      actionButton("setup_ready","Save Changes to R & Exit Upload", class = "btn-primary")
+      
+    ),
+    # Main panel for displaying outputs ----
+    mainPanel(
+      h4("Choice Data (first 10 rows):"),
+      h6(tableOutput("file1")),
+      h4("Attribute Coding from Excel:"),
+      h6(tableOutput("file2")),
+      h4("Optional Covariate Data (first 10 rows):"),
+      h6(tableOutput("file3"))
+    )
+  )
+)
+
+
+env_shiny$server_1 <- function(input, output) {
+  options(shiny.maxRequestSize=1000*1024^2)
+  data1 <- reactive({
+    if (is.null(input$file_conjoint)) {
+      result <- NULL
+    } else {
+      data_file <- input$file_conjoint$datapath
+      ftype <- toupper(substr(data_file,nchar(data_file)-2, nchar(data_file)))
+      if (ftype %in% c("CSV", "RDS")){
+        if (ftype == "CSV") {result <- read.csv(data_file, as.is=TRUE, check.names = FALSE)}
+        if (ftype == "RDS") {result <- readRDS(data_file)}
+      }  
+    } 
+    return(result)
+  })
+  
+  data2 <- reactive({
+    if (is.null(input$file_specs)) {
+      result <- NULL
+    } else{
+      result <- list()
+      result$specs_att_coding <- data.frame(read_xlsx(input$file_specs$datapath,
+                                                      sheet = "Att_Coding", col_types = c("text","text","numeric")))
+      result$specs_pair_constraints <- data.frame(read_xlsx(input$file_specs$datapath,
+                                                            sheet = "Pair_Constraints", col_types = c("text","numeric","numeric")))
+      result$specs_cov_coding <- data.frame(read_xlsx(input$file_specs$datapath,
+                                                      sheet = "Cov_Coding", col_types = c("text","text")))
+    } 
+    return(result)
+  })
+  
+  data3 <- reactive({
+    if (is.null(input$file_cov)) {
+      result <- NULL
+    } else {
+      data_file <- input$file_cov$datapath
+      ftype <- toupper(substr(data_file,nchar(data_file)-2, nchar(data_file)))
+      if (ftype %in% c("CSV", "RDS")){
+        if (ftype == "CSV") {result <- read.csv(data_file, as.is=TRUE, check.names = FALSE)}
+        if (ftype == "RDS") {result <- readRDS(data_file)}
+      }
+    }   
+    return(result)
+  })
+  
+  output$file1 <- renderTable({data1()[1:10,]})
+  output$file2 <- renderTable({data2()$specs_att_coding})
+  output$file3 <- renderTable({data3()[1:10,]})
+  
+  observeEvent(input$setup_ready, {
+    if (!is.null(data1())) {.GlobalEnv$data_conjoint <- data1()}
+    if (!is.null(data2())) {
+      .GlobalEnv$specs_att_coding <- data2()$specs_att_coding
+      .GlobalEnv$specs_pair_constraints <- data2()$specs_pair_constraints
+      .GlobalEnv$specs_cov_coding <- data2()$specs_cov_coding
+    }
+    if (input$use_cov){
+      .GlobalEnv$data_cov <- data3()
+    } else .GlobalEnv$data_cov <- NULL
+    .GlobalEnv$check_collinearity <- input$check_collinearity # May take a few minutes to check if your coding is deficient
+    .GlobalEnv$est_aggmodel <- input$est_aggmodel # Option to estimate aggregate model
+    stopApp()
+  })
+}
 # Do not attach multiple times -- detach first
 attach(env_code)
 attach(env_modfun)
 attach(env_eb)
 attach(env_stan)
-
+attach(env_shiny)
 
