ae81ad97c8cb5e491fe3347504f1a600e51cef80
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2025-04-15 17:35:26 -0400

    Update plot_draws to allow no plotting

--- EE_Functions_Stan4.7.R
+++ EE_Functions_Stan4.7.R
@@ -1050,7 +1050,8 @@ env_stan$create_tempdir <- function(dir, out_folder, out_prefix, stan_outname, s
 
 
 env_stan$plot_draws_df <- function(draws, vnames = NULL, ylab = "Draw", pdf_path = NULL,
-                                   chain_colors = rep(c("red","blue","green","black"),2)){
+                                   chain_colors = rep(c("red","blue","green","black"),2),makepdf=NULL){
+  if (is.null(makepdf)) makepdf <- TRUE
   # Assume draws is a list of data frames
   # Each data frame is draws for one chain of results with rows for iterations 
   # We plot each column
@@ -1062,7 +1063,7 @@ env_stan$plot_draws_df <- function(draws, vnames = NULL, ylab = "Draw", pdf_path
     rhat = NA,
     ESS = NA
   )
-  if (!is.null(pdf_path)){
+  if (!is.null(pdf_path) && makepdf){
     pdf(file = pdf_path,   # The directory you want to save the file in
         width = 7, # The width of the plot in inches
         height = 5) # The height of the plot in inches
@@ -1075,29 +1076,31 @@ env_stan$plot_draws_df <- function(draws, vnames = NULL, ylab = "Draw", pdf_path
     fit_stats$sd[i] <- round(sd(x),5)
     fit_stats$rhat[i] <- round(rhat(x),2)
     fit_stats$ESS[i] <- round(ess_basic(x),1)
-    plot(x[,1], type = "l", col = chain_colors[1], ylim = c(min(x), max(x)),
-         xlab = "Sample Iteration", ylab = ylab,
-         main = paste(vnames[i],
-                      "| rhat = ", round(rhat(x),2),
-                      "| ESS = ", round(ess_basic(x),1)
-         ))
-    if (ncol(x) > 1){
-      for (chain in 2:ncol(x)){
-        lines(x[,chain], type = "l", col = chain_colors[chain])
+    if (makepdf==TRUE){
+      plot(x[,1], type = "l", col = chain_colors[1], ylim = c(min(x), max(x)),
+           xlab = "Sample Iteration", ylab = ylab,
+           main = paste(vnames[i],
+                        "| rhat = ", round(rhat(x),2),
+                        "| ESS = ", round(ess_basic(x),1)
+           ))
+      if (ncol(x) > 1){
+        for (chain in 2:ncol(x)){
+          lines(x[,chain], type = "l", col = chain_colors[chain])
+        }
       }
     }
   } # end for
-  if (!is.null(pdf_path)) dev.off()
+  if (!is.null(pdf_path) && makepdf==TRUE) dev.off()
   return(fit_stats)
 }
 
-env_stan$check_draws_vector <- function(output_files, stan_vector, vector_labels = NULL, out_dir, out_prefix){
+env_stan$check_draws_vector <- function(output_files, stan_vector, vector_labels = NULL, out_dir, out_prefix,makepdf=NULL){
   # stan_vector is vector/1D array or scalar
   # vector labels are names for each item in vector, NULL auto assigns
   draws_stan_var <- read_cmdstan_csv(output_files, variables = stan_vector, format = "draws_list")
   draws_stan_df <- lapply(draws_stan_var$post_warmup_draws, function(x) do.call(cbind, x))
   fit_stats_var <- plot_draws_df(draws_stan_df, vector_labels, toupper(stan_vector),
-                                 pdf_path = file.path(out_dir,paste0(out_prefix,"_trace_",stan_vector,".pdf")))
+                                 pdf_path = file.path(out_dir,paste0(out_prefix,"_trace_",stan_vector,".pdf")),makepdf=makepdf)
   write.table(fit_stats_var, file = file.path(out_dir, paste0(out_prefix,"_fit_stats_", stan_vector,".csv")), sep = ",", na = ".", row.names = FALSE)
 } 
 
