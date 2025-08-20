b492235d20f9cd302dccb52a3745972648de9953
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2025-08-20 17:01:39 -0500

    Update EE_Functions_Stan6.0.R

--- EE_Functions_Stan6.0.R
+++ EE_Functions_Stan6.0.R
@@ -1727,7 +1727,7 @@ env_stan$LC_est <- function(data_stan, data_model, out_folder, stanout, out_pref
       LC_utilities_no0 <- LC_utilities[,-1] # remove LC segment with all 0 utilities
       # Combine outputs
       LC_LL <- c(LC_LL, LC_lp)
-      LC_betas <- cbind(LC_betas,LC_utilities)
+      LC_betas <- cbind(LC_betas,LC_utilities_no0)
       LC_probs0 <- cbind(LC_probs0,resp_seg_prob_posterior[,1])
       LC_probsno0 <- cbind(LC_probsno0,resp_seg_x0)
       LC_pred <- cbind(LC_pred, pred)
@@ -1740,7 +1740,7 @@ env_stan$LC_est <- function(data_stan, data_model, out_folder, stanout, out_pref
     ))
     print(as.data.frame(LC_LL))
     write.table(LC_probsno0, file = file.path(dir_run, paste0(out_prefix,"_LC_probs.csv")), sep = ",", na = ".", row.names = FALSE)
-    result <- list(LC_LL = LC_LL, LC_betas = LC_betas_no0, LC_probs = LC_probsno0, LC_probs0 = LC_probs0, LC_pred = LC_pred, LC_prob_good = 1 - apply(LC_probs0[,-1],1,min))
+    result <- list(LC_LL = LC_LL, LC_betas = LC_betas, LC_probs = LC_probsno0, LC_probs0 = LC_probs0, LC_pred = LC_pred, LC_prob_good = 1 - apply(LC_probs0[,-1],1,min))
     saveRDS(result, file.path(dir_run, paste0(out_prefix, "_LC_fit.rds")))
   } else result <- NULL
   return(result)
