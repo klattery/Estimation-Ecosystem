363f01d3b8629c12a02c1627bafcacce6da02fbd
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2025-08-26 15:30:07 -0500

    Update EE_Functions_Stan6.0.R

--- EE_Functions_Stan6.0.R
+++ EE_Functions_Stan6.0.R
@@ -1743,7 +1743,7 @@ env_stan$LC_est <- function(data_stan, data_model, out_folder, stanout, out_pref
     ))
     print(as.data.frame(LC_LL))
     write.table(LC_probsno0, file = file.path(dir_run, paste0(out_prefix,"_LC_probs.csv")), sep = ",", na = ".", row.names = FALSE)
-    LC_betas <- cbind(data.frame("att" = data_stan$code_master), data.frame(LC_betas))
+    LC_betas <- cbind(data.frame("att" = row.names(data_stan$code_master)), data.frame(LC_betas))
     write.table(LC_betas, file = file.path(dir_run, paste0(out_prefix,"_LC_betas.csv")), sep = ",", na = ".", row.names = FALSE)
     result <- list(LC_LL = LC_LL, LC_betas = LC_betas, LC_probs = LC_probsno0, LC_probs0 = LC_probs0, LC_pred = LC_pred, LC_prob_good = 1 - apply(LC_probs0[,-1],1,min))
     saveRDS(result, file.path(dir_run, paste0(out_prefix, "_LC_fit.rds")))
